3969e5af92ddc74766019753d8ab808022ee988c
yuj <nosae_reverse@163.com>
2022-05-23 17:31:05 +0800

    补全db操作中的事务操作

--- src/main/java/com/lx/config/IConfiguration.java
+++ src/main/java/com/lx/config/IConfiguration.java
@@ -1,14 +1,19 @@
 package com.lx.config;
 
+import io.r2dbc.spi.ConnectionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.r2dbc.connectionfactory.R2dbcTransactionManager;
 import org.springframework.data.r2dbc.core.DatabaseClient;
 import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
+import org.springframework.transaction.ReactiveTransactionManager;
+import org.springframework.transaction.reactive.TransactionalOperator;
 
 /**
  * @Author: jyu
  * @Date: 2022/5/13
  * @Description: 基础配置类
+ *
  **/
 @Configuration
 public class IConfiguration {
@@ -22,4 +27,24 @@ public class IConfiguration {
     public R2dbcEntityTemplate r2dbcEntityTemplate(DatabaseClient databaseClient){
         return new R2dbcEntityTemplate(databaseClient);
     }
+
+    /**
+     * 手动注册 TransactionalOperator
+     * 为什么能够注入 ReactiveTransactionManager?
+     * 因为 {@link org.springframework.boot.autoconfigure.data.r2dbc.R2dbcTransactionManagerAutoConfiguration} 中自动装配了 ReactiveTransactionManager
+     * @param transactionManager
+     * @return
+     */
+    @Bean
+    public TransactionalOperator transactionalOperator(ReactiveTransactionManager transactionManager) {
+        return TransactionalOperator.create(transactionManager);
+    }
+
+    // 如果没有上述 R2dbcTransactionManagerAutoConfiguration 中自动装配 ReactiveTransactionManager，则需要打开此注释手动装配
+    /*@Bean
+    public ReactiveTransactionManager transactionManager(ConnectionFactory connectionFactory) {
+        return (new R2dbcTransactionManager(connectionFactory));
+    }*/
+
+
 }
--- src/main/java/com/lx/controller/DBController.java
+++ src/main/java/com/lx/controller/DBController.java
@@ -11,16 +11,20 @@ import org.springframework.data.r2dbc.core.DatabaseClient;
 import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
 import org.springframework.data.relational.core.query.Criteria;
 import org.springframework.data.relational.core.query.Query;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.reactive.TransactionalOperator;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * @Author: jyu
  * @Date: 2022/5/13
- * @Description: 尝试了几种数据库的操作方式
+ * @Description: 尝试了几种数据库的操作方式，包含事务模式
  *
  * @see org.springframework.data.r2dbc.repository.R2dbcRepository
  * @see org.springframework.data.r2dbc.core.DatabaseClient;
@@ -38,7 +42,8 @@ public class DBController {
                 .GET(PATH_PREFIX + "repository", this::repository)
                 .GET(PATH_PREFIX + "databaseClient", this::databaseClient)
                 .GET(PATH_PREFIX + "template", this::template)
-                .GET(PATH_PREFIX + "transactionTest", this::transactionTest)
+                .GET(PATH_PREFIX + "testForTransactionalOperator", this::testForTransactionalOperator)
+                .GET(PATH_PREFIX + "testForTransactionManager", this::testForTransactionManager)
                 .build();
     }
 
@@ -46,6 +51,8 @@ public class DBController {
 //    private final R2dbcRepository<TestEntity,Integer> testEntityRepository;
     private final DatabaseClient databaseClient;
     private final R2dbcEntityTemplate r2dbcEntityTemplate;
+
+    private final TransactionalOperator transactionalOperator;
 
     public Mono<ServerResponse> template(ServerRequest serverRequest) {
         // select * from test_entity where entity_name='myName2' sort by entity_id desc
@@ -102,12 +109,57 @@ public class DBController {
     }
 
 
-    //todo 事务模式
+    // 事务模式
 
-    public Mono<ServerResponse> transactionTest(ServerRequest serverRequest){
+    private static final AtomicInteger flag = new AtomicInteger(0);
 
+    public Mono<ServerResponse> testForTransactionalOperator(ServerRequest serverRequest){
+        // 使用 transactionalOperator 示例
+        /**
+         * 注意这里只有第一次会成功，
+         * 因为 flag 第一次成功以后会变成1，第二次 flag 变成2的时候会抛出 “手动异常”，此时不仅数据库回滚，flag 的值也发生了回滚
+         */
+        return testEntityRepository
+                .findById(9)
+                .log()
+                .map(e -> {
+                    System.out.println(flag.get());
+                    if (flag.get()%2 == 1){
+                        throw new RuntimeException("手动异常!");
+                    }
+                    e.setEntityValue(e.getEntityValue() + flag.incrementAndGet());
+                    return e;
+                })
+                .flatMap(e -> testEntityRepository.save(e))
+                .flatMap(e -> ServerResponse.ok().bodyValue(e))
+                .as(transactionalOperator::transactional)
+        ;
+    }
 
-        return ServerResponse.ok().build();
+    /**
+     * 为什么这里的 transactionManager 能够使用 connectionFactoryTransactionManager？
+     * 因为 {@link org.springframework.boot.autoconfigure.data.r2dbc.R2dbcTransactionManagerAutoConfiguration} 中自动装配了 ReactiveTransactionManager
+     * @param serverRequest
+     * @return
+     */
+    // 如果是读操作，可以将以下 readOnly 注释解开
+    @Transactional(/*readOnly = true, */transactionManager = "connectionFactoryTransactionManager")
+    public Mono<ServerResponse> testForTransactionManager(ServerRequest serverRequest){
+        // 效果与 testForTransactionalOperator 方法所展示一致
+        return testEntityRepository
+                .findById(9)
+                .log()
+                .map(e -> {
+                    System.out.println(flag.get());
+                    if (flag.get()%2 == 1){
+                        throw new RuntimeException("手动异常!");
+                    }
+                    e.setEntityValue(e.getEntityValue() + flag.incrementAndGet());
+                    return e;
+                })
+                .flatMap(e -> testEntityRepository.save(e))
+                .flatMap(e -> ServerResponse.ok().bodyValue(e))
+                ;
     }
 
 
